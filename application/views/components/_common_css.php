eb3129af90f92c1e7e841d13af69d0c077aeb34c
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-10-13 18:26:27 +0530

    Headings Added

--- application/views/components/_common_css.php
+++ application/views/components/_common_css.php
@@ -6,5 +6,6 @@
   href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
 />
 
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
 <link rel="stylesheet" href="<?= base_url("assets/css/style.min.css") ?>">
--- application/views/components/_common_js.php
+++ application/views/components/_common_js.php
@@ -1,12 +1,22 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
 
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
 <script>
+    AOS.init();
+    
     $(function() {
-        $.scrollify({
-            section: "main section",
-            interstitialSection : "section.map-section, section.gandhi-quote, section#testimonials, section#contact-us, footer",
-            scrollSpeed: 50,
-        });
+
+        if (window.matchMedia('(min-width: 768px)').matches) {
+        }
+
+        if (window.matchMedia('(min-width: 1500px)').matches) {
+            $.scrollify({
+                section: "main section",
+                interstitialSection: "section.text, section.map-section, section.gandhi-quote, section#testimonials, section#about-us, section#contact-us, footer",
+                scrollSpeed: 50,
+            });
+        }
     });
 </script>
