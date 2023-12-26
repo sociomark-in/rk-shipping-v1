7dd9545c64c20e2cafce104fe2fa276606c8d8d6
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-12-26 11:45:30 +0530

    Reset Audip

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -103,15 +103,11 @@
 				<!-- <div class="swiper-scrollbar"></div> -->
 			</div>
 		</section>
-		<audio controls autoplay id="backgroundAudio" style="display:none">
+		<audio controls autoplay>
 			<source src="<?= base_url() ?>assets/media/audio.mp3" type="audio/mp3">
 			<source src="<?= base_url() ?>assets/media/audio.ogg" type="audio/ogg">
 			Your browser does not support the audio element.
 		</audio>
-		<script>
-			var audio = document.getElementById('backgroundAudio');
-			audio.play();
-		</script>
 		<section class="text_mtm d-none">
 			<div class="wrapper text-center">
 				<div class="container">
