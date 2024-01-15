f402a53c15abea02ee692baf6a339dc9fea87169
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-01-15 12:27:45 +0530

    text Change

--- application/views/components/_common_footer.php
+++ application/views/components/_common_footer.php
@@ -6,7 +6,7 @@
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Designed &amp; Developed by <!-- <a href="#" target="_blank" class="link">Hemant Karekar</a> from  --><a href="https://sociomark.in/?utm_source=<?= APP_LINK ?>&utm_medium=website_traffic&utm_campaign=direct_footer_click" target="_blank" class="link">Sociomark</a>
+                    Designed &amp; Developed by <!-- <a href="#" target="_blank" class="link">Hemant Karekar</a> from  --><a href="https://sociomark.in/?utm_source=<?= base_url() ?>&utm_medium=website_traffic&utm_campaign=direct_footer_click" target="_blank" class="link">Sociomark</a>
                 </div>
             </div>
         </div>
